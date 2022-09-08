cceb6103da350703fb2d78de9028588dec2e4cc5
goldbingo <50813220+goldbingo@users.noreply.github.com>
2022-09-08 18:38:23 +0800

    Revert to original

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -1,23 +1,14 @@
 #include "wizmote.h"
 
-#include "esphome/core/helpers.h"
-#include "esphome/core/log.h"
-#include "esphome/core/version.h"
-
 namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   
   static const char *const TAG = "wizmote";
-  ESP_LOGD(TAG, "Button pressed");
   
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence <= this->last_sequence_) {
-    ESP_LOGD(TAG, "Sequence rmpty");
+  if (wizmote.sequence <= this->last_sequence_
     return;
-  }
-  
-  ESP_LOGD(TAG, "Sequence ongoing...");
   
   this->last_sequence_ = wizmote.sequence;
   this->on_button_->trigger(wizmote);
