ae83089852260bc885793d8106315dbfa4a01958
goldbingo <50813220+goldbingo@users.noreply.github.com>
2022-09-08 18:48:41 +0800

    Update wizmote.cpp

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -7,7 +7,7 @@ void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   static const char *const TAG = "wizmote";
   
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence <= this->last_sequence_
+  if (wizmote.sequence <= this->last_sequence_)
     return;
   
   this->last_sequence_ = wizmote.sequence;
