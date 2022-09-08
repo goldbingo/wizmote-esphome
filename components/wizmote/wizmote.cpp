bd342d7e12c267326578666a1f5d5de32a59280b
goldbingo <50813220+goldbingo@users.noreply.github.com>
2022-09-08 09:44:52 +0800

    Update wizmote.cpp
    add debug message

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -1,8 +1,16 @@
 #include "wizmote.h"
 
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+#include "esphome/core/version.h"
+
 namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
+  
+  static const char *const TAG = "wizmote";
+  ESP_LOGD(TAG, "Debug message by bingo");
+  
   WizMotePacket wizmote = WizMotePacket::build(packet);
   if (wizmote.sequence <= this->last_sequence_)
     return;
